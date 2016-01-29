48752eef9038055e758ecb462667eb4ffbc92ed1
nickroberts404 <nickroberts404@gmail.com>
2016-01-29 17:00:04 -0600

    Implemented methods

--- client.js
+++ client.js
@@ -1,11 +1,14 @@
 const skyglass = require('./skyglass.js');
+const http = require('http');
 
-skyglass.getStars(handleStars);
-skyglass.getConstellation('Ori');
-skyglass.getConstellations();
-skyglass.addConnection('Tau');
-skyglass.removeConnection('Ori');
+skyglass.getStars(handleData);
+skyglass.getStars({con: 'And', mag: 3}, handleData);
+skyglass.getConstellation('Ori', handleData);
+skyglass.getConstellations(handleData);
+// skyglass.addConnection('Tau', [1224, 4545], handleData);
+// skyglass.removeConnection('Ori', [2333, 5666] handleData);
 
-function handleStars(err, data){
-	return true;
+function handleData(err, response, body){
+	if (err) console.error('SkyGlass Error: ', err);
+	else console.log('Data: ', body);
 }
--- skyglass.js
+++ skyglass.js
@@ -1,25 +1,53 @@
 // skyglass.js
 
 const request = require('request');
+const api = 'http://localhost:3000'
 
 module.exports = {
 	getStars: function(options, cb){
 		console.log('Getting stars...');
-		if (typeof options === 'function'){
-			console.log('Callback is options');
+		if (typeof options === 'function') {
+			// The options argument is really our callback
+			request(api+'/stars', options)
+		} else {
+			request(api+'/stars'+buildQuery(options), cb);
 		}
 	},
 	getConstellations: function(cb){
 		console.log('Getting constellations...');
+		request(api+'/constellations', cb);
 	},
 	getConstellation: function(con, cb){
 		console.log('Getting constellation '+con+'...');
+		request(api+'/constellations?con='+con, cb);
 	},
 	addConnection: function(con, connection, cb){
 		console.log('Adding connection to '+con+'...');
+		var body = { connection: connection, addConnection: true }
+		updateConnection(con, body, cb)
 	},
 	removeConnection: function(con, connection, cb){
 		console.log('Removing connection to '+con+'...');
+		var body = { connection: connection, addConnection: false }
+		updateConnection(con, body, cb)
 	}
+}
 
+function updateConnection(con, body, cb){
+	console.log(body);
+	request({
+		url: api+'/constellations?con='+con, 
+		method:'PUT', 
+		headers: {
+	        'Content-Type': 'application/json'
+    	},
+		body: JSON.stringify(body)});
+}
+
+function buildQuery(options){
+	var query = '?';
+	for (option in options) {
+		query += option+'='+options[option]+'&'
+	}
+	return query.substring(0, query.length - 1);
 }
